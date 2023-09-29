fa012b03016a2fc1101eb70cf8f919b7f9674112
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-09-29 14:54:11 +0530

    added

---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import NewsCard from '../NewsCard/NewsCard';
+import { Grow, Grid, Typography } from '@mui/material';
+import './styles.css';
+
+function NewsCards({ articles }) {
+  return (
+
+    <Grow in>
+
+      <Grid container alignItems='stretch' spacing={3} className='container'>
+
+        {
+          articles.map((article, i) => (
+            <Grid item xs={12} sm={6} md={4} ld={3} style={{ display: 'flex' }}>
+              <NewsCard article={article} i={i} />
+            </Grid>
+          ))
+        }
+
+      </Grid>
+
+    </Grow>
+
+  )
+}
+
+export default NewsCards
