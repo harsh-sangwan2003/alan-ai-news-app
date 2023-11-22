237d7a4886d50edc4f124eb3ed73c4647eeb884c
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-11-22 19:18:49 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import NewsCards from './components/NewsCards/NewsCards'
+
+const alanKey = "abc1b1493c9f5bb0ef3d3248c4e73ceb2e956eca572e1d8b807a3e2338fdd0dc/stage"
 
 const App = () => {
+
+    const [newsArticles, setNewsArticles] = useState([]);
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if (command === 'newHeadlines')
+                    setNewsArticles(articles);
+            }
+        })
+    }, [])
+
     return (
         <div>
             <h1>Alan AI News App</h1>
+            <NewsCards articles={newsArticles}/>
         </div>
     )
 }
